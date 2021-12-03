6ec53e8b20ad66ed458e1fa8b17009a37439a486
Christian Hoffmann <hc@osz-lise-meitner.eu>
2021-12-03 11:15:37 +0100

    GPL-Preambel hinzugefügt.

--- items.py
+++ items.py
@@ -1,10 +1,25 @@
 
+# Endlostafel - Ein einfaches Schreibprogramm für interaktive Tafeln
+# Copyright (C) 2021  Christian Hoffmann
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see https://www.gnu.org/licenses/.
+
 from math import sqrt, log10
 from PySide6.QtCore import QPointF, Qt
 from PySide6.QtGui import QBrush, QColor, QPainterPath, QPalette, QPen, QPixmap
 from PySide6.QtSvgWidgets import QGraphicsSvgItem
 from PySide6.QtWidgets import QApplication, QGraphicsEllipseItem, QGraphicsItem, QGraphicsLineItem, QGraphicsPathItem, QGraphicsPixmapItem, QGraphicsRectItem, QGraphicsView
-
 
 
 class Pfad(QGraphicsPathItem):
